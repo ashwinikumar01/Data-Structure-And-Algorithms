0629c1990c192d05ac20e57236fb6231a466d83a
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-24 15:29:32 +0530

    Recursive Search Added

---
+++ Linked List/recursive_searching.c
@@ -0,0 +1,49 @@
+/*Using Linear Search*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+struct Node *RSearch(struct Node *p, int key)
+{
+    if (p == NULL)
+        return 0;
+    if (key == p->data)
+        return (p);
+    return RSearch(p->next, key);
+}
+
+int main()
+{
+    struct Node *temp;
+    int A[]={10,12,14,45,29};
+    create(A,5);
+    temp=RSearch(first,12);
+    if(temp)
+    printf("Key is Found %d\n",temp->data);
+    return 0;
+}
