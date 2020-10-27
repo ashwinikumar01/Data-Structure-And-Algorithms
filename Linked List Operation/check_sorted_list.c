361cb55f0d35ba78edb73822247aca4dd5282929
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-27 10:58:26 +0530

    Check linked list is sorted or not

---
+++ Linked List Operation/check_sorted_list.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int CheckSortedInsert(struct Node *p)
+{
+    int x = -65536;
+    while (p != NULL)
+    {
+        if (p->data < x)
+            return 0;
+        x = p->data;
+        p = p->next;
+    }
+    return 1;
+}
+
+int main()
+{
+    int A[] = {12, 45, 67, 89, 100};
+    create(A, 5);
+    Display(first);
+    printf("\n");
+    printf("Linked list is sorted =%d", CheckSortedInsert(first));
+
+    return 0;
+}
